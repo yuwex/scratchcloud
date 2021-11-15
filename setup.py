5e28d0ef03a6cebf83f9e7e1fc96f3dcf9889b38
yuwe <25470828+yuwex@users.noreply.github.com>
2021-11-15 14:49:56 -0800

    added setup for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as fin:
+        return fin.read()
+
+packages = [
+    'scratchcloudpy',
+    'scratchcloudpy.ext',
+]
+
+setup(
+    name='scratchcloudpy',
+    version='1.0.0',
+    author='yuwex',
+    url='https://github.com/yuwex/scratchcloudpy',
+    description='An asynchronous wrapper for scratch.mit.edu cloud variables',
+    long_description=readme(),
+    long_description_type='text/markdown',
+    install_requires=['aiohttp', 'requests', 'websockets'],
+    keywords=['core', 'package'],
+    packages=packages,
+)
