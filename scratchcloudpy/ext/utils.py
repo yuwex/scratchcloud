fee69166a022949933ac24391181f9be4aa8d537
yuwe <25470828+yuwex@users.noreply.github.com>
2021-11-29 22:45:10 -0800

    added docstrings

--- scratchcloudpy/ext/utils.py
+++ scratchcloudpy/ext/utils.py
@@ -7,18 +7,51 @@ from ..client import CloudClient, SizeError
 class MissingCloudVariable(Exception): pass
 
 class SegmentDump:
-    def __init__(self, client: CloudClient, cloud_names: list):
+    """A utility for sending and reading large data.
+    This class reads large data by breaks it into segments.
+    
+    :param client: The client that will be used for operations
+    :type client: :class:`client.CloudClient`
+    :param cloud_names: The cloud variables that will be set and read
+    :type cloud_names: List[str]
+
+    :raises MissingCloudVariable: If not all cloud_names are found in the cached cloud variables
+    """
+
+    def __init__(self, client: CloudClient, cloud_names: List[str]):
         self.client = client
         self.cloud_names = cloud_names
         
         self.MAX_CLOUD_LENGTH: int = 256
 
-    def dict_has_all_keys(self, d: dict, keys: List['str']):
+        if not self.dict_has_all_keys(self.client.cloud_variables, self.cloud_names):
+            raise MissingCloudVariable('cloud_names not found in project')
+
+    def dict_has_all_keys(self, d: dict, keys: List[str]):
         return not any([key not in d for key in keys])
 
     async def dump(self, data: str, encode_data: bool = False, delay: float = 0.01, empty_value: str = '0', encode_empty: bool = False):
-        if not self.dict_has_all_keys(self.client.cloud_variables, self.cloud_names):
-            raise MissingCloudVariable('cloud_names not found in project')
+        """A asynchronous method to dump data to several variables at once.
+        
+        :param data: The data that will be sent
+        :type data: str
+        :param encode_data: A boolean that encodes the data if true,
+            default false
+        :type encode_data: bool
+        :param delay: The delay between setting different cloud variables,
+            default 0.01
+        :type delay: float
+        :param empty_value: The value that unused segments are set to,
+            default '0'
+        :type empty_value: str
+        :param encode_empty: A boolean that encodes the empty value,
+            default False
+        :type encode_empty: bool
+
+        :raises ValueError: If the value that will be encoded is not digits
+        :raises SizeError: If the data that will be sent is larger than the maximum length
+        """
+
         
         if self.client.encoder and encode_data:
             data = self.client.encoder(data)
@@ -27,7 +60,7 @@ class SegmentDump:
             empty_value = self.client.encoder(empty_value)
 
         if not empty_value.isdigit():
-            raise TypeError('empty_value must digits')
+            raise ValueError('empty_value must digits')
 
         if len(data) > (self.MAX_CLOUD_LENGTH * len(self.cloud_names)):
             raise SizeError(f'Your data is too big! Must be less than or equal to {(self.MAX_CLOUD_LENGTH * len(self.cloud_names))} characters.')
@@ -48,14 +81,26 @@ class SegmentDump:
         return segments
             
     def read(self, decode_data: bool = False, end_var_value: str = '0', encode_end: bool = False) -> str:
-        if not self.dict_has_all_keys(self.client.cloud_variables, self.cloud_names):
-            raise MissingCloudVariable('cloud_names not found in project')
+        """A method to read data from several variables at once.
+        
+        :param decode_data: A boolean that decodes the data if true,
+            default False
+        :type decode_data: bool
+        :param end_var_value: The value of empty data
+            default '0'
+        :type end_var_value: str
+        :param encode_end: A boolean that encodes end_var_value before searching for it,
+            default false
+        :type encode_end: bool
+
+        :rtype: str
+        """
+
         
         payload = ''
         if self.client.encoder and encode_end:
             end_var_value = self.client.encoder(end_var_value)
         
-        print(self.client.cloud_variables)
         for name in self.cloud_names:
             value = self.client.cloud_variables[name]
             if value == end_var_value: break
