cc0a2b91499c2e8fe5dbdb42c77fdc4c92e72829
yuwex <25470828+yuwex@users.noreply.github.com>
2021-12-30 00:37:36 -0800

    Changed description

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     version='1.0.0',
     author='yuwex',
     url='https://github.com/yuwex/ScratchCloud',
-    description='An asynchronous wrapper for scratch.mit.edu cloud variables',
+    description='An event-based asynchronous wrapper for scratch.mit.edu cloud variables',
     long_description=readme(),
     long_description_type='text/markdown',
     install_requires=['aiohttp', 'websockets'],
