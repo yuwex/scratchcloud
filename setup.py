f5aa612b0a1c2c5b351a55982bd1271f60897eb9
yuwex <25470828+yuwex@users.noreply.github.com>
2022-01-17 19:07:32 -0800

    updated setuptools for PyPi, changed version from 0.0.1. Version will be changed to 1.0.0 after successful PyPi upload.

--- setup.py
+++ setup.py
@@ -4,20 +4,46 @@ def readme():
     with open('README.md') as fin:
         return fin.read()
 
+install_requires = [
+    'aiohttp>=3.8.0',
+    'websockets>=10.1'
+]
+
 packages = [
     'ScratchCloud',
     'ScratchCloud.ext',
 ]
 
+extras_require = {
+    'docs': [
+        'sphinx==4.3.2',
+        'sphinx-rtd-theme==1.0.0',
+    ]
+}
+
 setup(
     name='ScratchCloud',
-    version='1.0.0',
+    version='0.0.1',
     author='yuwex',
     url='https://github.com/yuwex/ScratchCloud',
-    description='An event-based asynchronous wrapper for scratch.mit.edu cloud variables',
+    description='An event-based asynchronous wrapper for scratch.mit.edu cloud variables.',
     long_description=readme(),
     long_description_type='text/markdown',
-    install_requires=['aiohttp', 'websockets'],
-    keywords=['core', 'package'],
+    
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires='>=3.8.0',
     packages=packages,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Education',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries'
+    ]
 )
