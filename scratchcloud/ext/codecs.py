07620ae5ee8d6f0d5cb63e62720d8358b1827c7f
yuwex <25470828+yuwex@users.noreply.github.com>
2022-01-20 16:54:43 -0800

    Updated BaseCodec
    M Renamed alphabet --> plainalpha
    + BaseCodec takes str and list types for plainalpha
    + Offset param, allows users to specify where the encode list starts (default 10)
    + force_lowercase bool, allows users to specify if all text will be lowered
    + places_per_character, allows users to encode/decode for characters that are n length rather than length 2
    + Docstrings

--- scratchcloud/ext/codecs.py
+++ scratchcloud/ext/codecs.py
@@ -2,26 +2,84 @@ from textwrap import wrap
 from ..errors import EncodeError, DecodeError
 
 class BaseCodec:
-    def __init__(self, alphabet: str = 'abcdefghijklmnopqrstuvwxyz1234567890 -=[]\\;\',./!@#$%^&*()_+{}|:\"<>?'):
-        self.plainalpha = list(alphabet)
+    """A codec that encodes and decodes data to and from n-length numbers.
+    
+    :param plainalpha: The alphabet that will be used for encoding to number
+    :type plainalpha: str | list[str]
+    :param offset: The number where cipheralpha will start
+    :type offset: int
+    :param force_lowecase: If all characters should be forced lowercase
+    :type force_lowecase: bool
+    :param places_per_character: the length of each item in cipher ints. Numbers in cipheralpha smaller than this value will begin with 0s.
+    :type places_per_character: int
+
+    :raises TypeError: If Cipheralpha has too large numbers.
+    """
+
+    def __init__(self, plainalpha: str | list[str] = 'abcdefghijklmnopqrstuvwxyz1234567890 -=[]\\;\',./!@#$%^&*()_+{}|:\"<>?', offset: int = 10, force_lowercase: bool = False, places_per_character: int = 2):
+        self.force_lowercase = force_lowercase
+        self.places_per_character = places_per_character
         
-        cipher_ints = list(range(10, len(self.plainalpha) + 10))
-        self.cipheralpha = [str(v) for v in cipher_ints]
+        if isinstance(plainalpha, list):
+            self.plainalpha = plainalpha
+        else:
+            self.plainalpha = list(plainalpha)
+
+        self.cipheralpha = []
+
+        cipher_ints = list(range(offset, len(self.plainalpha) + offset))
+        for number in cipher_ints:
+            number = str(number)
+
+            if len(number) > self.places_per_character:
+                raise TypeError('Cipheralpha has too many numbers. Remove characters from the specified alphabet or increase places_per_character.')
+            elif len(number) != self.places_per_character:
+                zeros = (self.places_per_character - len(number)) * '0'
+                self.cipheralpha.append(f'{zeros}{number}')
+            else:
+                self.cipheralpha.append(number)        
 
     def encode(self, plaintext: str) -> str:
+        """A method that encodes plaintext.
+
+        :param plaintext: The text that will be decoded
+        :type plaintext: str
+
+        :raises EncodeError: If unable to encode. Likely due to characters not included in plainalpha.
+
+        :rtype: str
+        """
+
+        if self.force_lowercase:
+            plaintext = plaintext.lower()
+        
         try:
             encoded = []
-            for letter in plaintext.lower():
+            for letter in plaintext:
                 letter = self.cipheralpha[self.plainalpha.index(letter)]
                 encoded.append(letter)
             return ''.join(encoded)
         except Exception as e:
             raise EncodeError(f'Unable to encode: {plaintext} due to \n{e}')
 
-    def decode(self, ciphertext: str) -> str:
+    def decode(self, ciphertext: str | int) -> str:
+        """A method that decodes ciphertext.
+
+        :param ciphertext: The text that will be encoded
+        :type ciphertext: str | int
+
+        :raises EncodeError: If unable to encode. Likely due to characters not included in plainalpha.
+
+        :rtype: str
+        """
+
+
+        if self.force_lowercase:
+            ciphertext = ciphertext.lower()
+        
         try:
             decoded = []
-            for letter in wrap(ciphertext.lower(), 2):
+            for letter in wrap(ciphertext, self.places_per_character):
                 letter = self.plainalpha[self.cipheralpha.index(letter)]
                 decoded.append(letter)
             return ''.join(decoded)
