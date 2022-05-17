e686286a77dc73f8ed566c310a6c285919568ecb
yuwex <25470828+yuwex@users.noreply.github.com>
2022-05-16 19:07:44 -0700

    ver 1.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras_require = {
 
 setup(
     name='scratchcloud',
-    version='1.2.0',
+    version='1.2.1',
     author='yuwex',
     url='https://github.com/yuwex/scratchcloud',
     description='An event-based asynchronous wrapper for scratch.mit.edu cloud variables.',
